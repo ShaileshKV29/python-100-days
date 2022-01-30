98b807da7a7678c4d608f0854de8b6ee0208eede
ShaileshKV29 <shailesh29062001@gmail.com>
2022-01-30 22:53:51 +0530

    File Handling

---
+++ Day24 - File Handling/Mail Merge Project Start/main.py
@@ -0,0 +1,23 @@
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+starting_letter = open('Day24 - File Handling/Mail Merge Project/Input/Letters/starting_letter.txt')
+old_letter = starting_letter.read()
+names_file = open('Day24 - File Handling/Mail Merge Project/Input/Names/invited_names.txt')
+
+names = names_file.readlines()
+for name in names:
+    output_file = open(f'Day24 - File Handling/Mail Merge Project/Output/ReadyToSend/send-to-{name[:-1]}.txt', "w")
+    print(name[:-1])
+
+
+starting_letter.close()
+names_file.close()
+
+    

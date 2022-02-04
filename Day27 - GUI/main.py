eac3021024966856e5030608adc08c4e3d363051
ShaileshKV29 <shailesh29062001@gmail.com>
2022-02-04 22:31:57 +0530

    Tkinter Label

--- Day27 - GUI/main.py
+++ Day27 - GUI/main.py
@@ -1,7 +1,13 @@
 import tkinter
 
+from matplotlib.pyplot import title
+
 window = tkinter.Tk()
 window.title("First GUI Program, Not Technically")
 window.minsize(width=500, height=300)
+
+# Label
+my_label = tkinter.Label(text="Here is a Label")
+my_label.pack()
 
 window.mainloop()
